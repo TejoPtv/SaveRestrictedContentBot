02aa6e799bc3b474d5c0d0b10ab77f9259877152
TejoPtv <134220546+TejoPtv@users.noreply.github.com>
2023-06-15 13:53:02 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+API_ID = config("API_ID"16995961, default=None, cast=int)
+API_HASH = config("API_HASH"8817a7d4293049593e60999359970ddd , default=None)
+BOT_TOKEN = config("BOT_TOKEN"6217840102:AAGXJ0yMrPT_edRmUkSVWJFEA3e3LikZVCw, default=None)
+SESSION = config("SESSION"BQCmluXZkyv6Bjl6eCA6gX0Xex7XX1rhhxcd2gYt1Xc7DHXg4ccGUFqKMLgDo_be93jWymr8EP1uIvwpCunkkT1fEkiHY0ig4UEf-N8HfCQQqpgYr6fOMKkojTy2C8vYfJBLnSbJ4Vp25b7bMyapS0jK_oto1PPsVIAk-eCi6OhHYWGJo2jQ205UkJhpInBlBokvmzxcP07v8WtAhgQMyNci9XX5esIR8NVBzhfzfQFX4CLauC9ndKWccyS6ILhhyzJEJkTX7McW0XdMgXVl9R1oLoLmpTnxI4zEmCbt6eXJjzSSCiCbn7yibaAhRbCuHvfq3uxhxHoDLyops76syJbEYcxeBwA, default=None)
+FORCESUB = config("FORCESUB"t.me/+2Hg_3iFLjkVmYmFl ,default=None) 
+
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
